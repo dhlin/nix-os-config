39898f16764eb0e407d1b3fc7d02d77730eb7ab3
Donghang Lin <donghanglin@gmail.com>
2021-07-18 17:08:09 -0700

    add fzf key bindings

--- .zinit.zsh
+++ .zinit.zsh
@@ -2,3 +2,7 @@ zinit ice depth"1"
 zinit light romkatv/powerlevel10k
 
 zinit light zsh-users/zsh-autosuggestions
+
+if which -s fzf > /dev/null; then
+    zinit snippet https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh
+fi
