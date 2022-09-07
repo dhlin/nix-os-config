ea006400adb60f2fa3dcc42cea9d2e9fa1e014bf
Donghang Lin <donghanglin@gmail.com>
2022-09-06 20:53:48 -0700

    setup fzf, zoxide via zinit

--- .zinit.zsh
+++ .zinit.zsh
@@ -3,6 +3,15 @@ zinit light romkatv/powerlevel10k
 
 zinit light zsh-users/zsh-autosuggestions
 
-if which -s fzf > /dev/null; then
-    zinit snippet https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh
-fi
+zinit from"gh-r" \
+    as"program" \
+    for junegunn/fzf
+
+zinit snippet https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh
+
+zinit from"gh-r" \
+    as"program" \
+    atclone"./zoxide init zsh > zoxide.zsh" \
+    atpull"%atclone" \
+    pick"zoxide" src="zoxide.zsh" \
+    for ajeetdsouza/zoxide
