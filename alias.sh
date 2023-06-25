a185ef9fa435fb80c496ee28b0a83d304cd135a6
Donghang Lin <donghanglin@gmail.com>
2023-06-25 16:51:53 +0000

    add new aliases

--- alias.sh
+++ alias.sh
@@ -10,15 +10,20 @@ alias drm='docker rm'
 alias drp='dr --privileged'
 
 # git
+alias gau='git add -u'
 alias gb='git branch'
 alias gca='git commit --amend'
 alias gco='git checkout'
 alias gcp='git cherry-pick'
 alias gd='git diff'
 alias gdc='git diff --cached'
-alias gfo='git fetch origin' 
+alias gfo='git fetch origin'
+alias gl='git log'
+alias grs='git restore --staged'
+alias grv='git remote -vv'
 alias gp='git pull origin $(git rev-parse --abbrev-ref HEAD)'
 alias gs='git status'
 
-#
+# misc
 alias ll='ls -laG'
+alias s='source'
