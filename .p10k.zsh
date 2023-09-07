a07f3fd98d4676a3fba47149cc591080f339be11
Donghang Lin <donghanglin@gmail.com>
2023-09-06 20:37:23 -0700

    enable more powerlevel10k left prompt

--- .p10k.zsh
+++ .p10k.zsh
@@ -31,11 +31,11 @@
 
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    # os_icon               # os identifier
+    os_icon                 # os identifier
     context                 # user@hostname
     dir                     # current directory
     vcs                     # git status
-    # prompt_char           # prompt symbol
+    prompt_char             # prompt symbol
   )
 
   # The list of segments shown on the right. Fill it with less important segments.
