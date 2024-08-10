052b060220eaef077a55f926a8cb6ef773d708af
Donghang Lin <donghanglin@gmail.com>
2024-08-10 08:25:24 -0700

    move custom aliases to custom folder
    The nix-os-config folder now maintains common setup for the systems.
    Custom setup is separated into the custom folder. External config
    can refer to it via "${nix-os-config}/custom/*.nix".

--- nix-os-config/home-config.nix
+++ nix-os-config/home-config.nix
@@ -88,36 +88,10 @@
         }
       ];
       shellAliases = {
-        # docker
-        de = "docker exec -it";
-        dep = "de --privileged";
-        di = "docker images";
-        dirm = "docker image rm";
-        dp = "docker ps";
-        dpa = "docker ps -a";
-        dr = "docker run -it --rm";
-        drm = "docker rm";
-        drp = "dr --privileged";
-
-        # git
-        gau = "git add -u";
-        gb = "git branch";
-        gca = "git commit --amend";
-        gco = "git checkout";
-        gcp = "git cherry-pick";
-        gd = "git diff";
-        gdc = "git diff --cached";
-        gfo = "git fetch origin";
-        gl = "git log";
-        grs = "git restore --staged";
-        grv = "git remote -vv";
-        gp = "git pull origin $(git rev-parse --abbrev-ref HEAD)";
-        gs = "git status";
-
         # file
         ll = "ls -laG";
         ".." = "cd ..";
-        "..." = "cd ...";
+        "..." = "cd ../..";
       };
     };
   };
